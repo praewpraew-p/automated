c1b61b1b4163b2cbba4354ccce590c777da36a92
Arisara Phongsai <aris.pw10@gmail.com>
2025-03-01 09:53:48 +0700

    init eventpop

---
+++ tests/eventpop.spec.ts
@@ -0,0 +1,31 @@
+import test, { expect } from "@playwright/test";
+
+// eventpop.queue-it.net/?c=eventpop&e=77578fort&cid=en-US
+const home_url = "https://www.eventpop.me/";
+const queue_url = "https://queue.eventpop.me/prequeue/th/?id=79097";
+
+test("queueing", async ({ page }) => {
+  await page.goto(home_url);
+  await page.getByRole("button", { name: "That's ok" }).click();
+  await page
+    .getByRole("link", { name: "Log In / Sign Up", exact: true })
+    .click();
+  await page.getByLabel("* Email").fill("qqwra.3@gmail.com");
+  await page.getByLabel("* Password", { exact: true }).fill("p0837968799");
+  await page.getByRole("button", { name: "Log In" }).click();
+  await page.waitForTimeout(2000);
+
+  await page.goto(queue_url);
+
+  while (true) {
+    try {
+      const queueButton = await page.getByRole("link", {
+        name: "เข้ารับคิว",
+      });
+      await queueButton.click();
+      await page.waitForTimeout(100);
+    } catch (error) {
+      await page.pause();
+    }
+  }
+});
